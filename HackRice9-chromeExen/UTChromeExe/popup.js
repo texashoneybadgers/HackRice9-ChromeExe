e3801b4a397e7bebf7f941ed2889611b48e62193
Chao07 <55027220+Chao07@users.noreply.github.com>
2019-09-21 20:47:27 -0500

    Add files via upload

---
+++ HackRice9-chromeExen/UTChromeExe/popup.js
@@ -0,0 +1,10 @@
+  
+  document.addEventListener('DOMContentLoaded', function() {
+
+    var checkPageButton = document.getElementById('checkPage');
+  
+    checkPageButton.addEventListener('click', function() {
+       window.open('pngPop.html');
+    }, false);
+    
+  }, false);
